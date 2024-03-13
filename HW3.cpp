55e4814a2576f21a0cfb4a08a5b1eb9155b701e7
Limbodancer23 <83474450+Limbodancer23@users.noreply.github.com>
2024-03-13 23:40:05 +0500

    Добавьте файлы проекта.

---
+++ HW3.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string str = "Hello";
+    cout << str.length() << endl;
+    cout << str.back() << endl;
+    cout << str[0] << endl;
+}
